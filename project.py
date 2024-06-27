45cc3b0fc0f1c94c98a3406094eda3d0fdde1841
TobiasBelling <Tobias.Belling@hotmail.com>
2024-06-27 17:18:14 +0200

    enigma machine works now

--- project.py
+++ project.py
@@ -1,36 +1,39 @@
+# TODO everything works, now make input and output
+
 from keyboard import Keyboard
 from plugboard import Plugboard
 from rotor import Rotor
 from reflector import Reflect
+from enigma import Enigma
 
-
+# historicly accurate enigma components taken from wikipedia: https://en.wikipedia.org/wiki/Enigma_rotor_details
 I = Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q")
 II = Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E")
 III = Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V")
 IV = Rotor("ESOVPZJAYQUIRHXLNFTGKDCMWB", "J")
 V = Rotor("VZBRGITYUPSDNHLXAWMJQOFECK", "Z")
-
 A = Reflect("EJMZALYXVBWFCRQUONTSPIKHGD")
 B = Reflect("YRUHQSLDPXNGOKMIEBFZCWVJAT")
 C = Reflect("FVPJIAOYEDRZXWGCTKUQSBNMHL")
 
 keyboard = Keyboard()
-plug = Plugboard(["AR", "GK", "OX"])
+
+#plugboard settings. TODO: give user acces to change plugboard settings
+plug = Plugboard(["AB", "CD", "EF"])
+
+# define Enigma machine. TODO: allow user to input own combination of rotors and reflectors
+ENIGMA = Enigma(B,IV,II,I,plug,keyboard)
 
 def main():
-    letter = "A"
-    signal = keyboard.forward(letter)
-    signal = plug.forward(signal)
-    signal = III.forward(signal)
-    signal = II.forward(signal)
-    signal = I.forward(signal)
-    signal = A.reflect(signal)
-    signal = I.backward(signal)
-    signal = II.backward(signal)
-    signal = III.backward(signal)
-    signal = plug.backward(signal)
-    letter = keyboard.backward(signal)
-    print(letter)
+    ENIGMA.set_key("CAT")
+
+    ENIGMA.set_rings((5,26,2))
+
+    message = "TESTINGTESTINGTESTINGTESTING"
+    cipher_text = ""
+    for letter in message:
+        cipher_text = cipher_text + ENIGMA.encipher(letter)
+    print(cipher_text)
     
 
 if __name__ == "__main__":
--- rotor.py
+++ rotor.py
@@ -1,4 +1,3 @@
-# link to wikipedia with historical rotors: https://en.wikipedia.org/wiki/Enigma_rotor_details
 class Rotor:
 
     def __init__(self, wiring, notch):
@@ -15,3 +14,30 @@ class Rotor:
         letter = self.left[signal]
         signal = self.right.find(letter)
         return signal
+    
+    def show(self):
+        print(self.left)
+        print(self.right)
+        print("")
+    
+    def rotate(self, n = 1, forward = True):
+        for i in range(n):
+            if forward:
+                self.left = self.left[1:] + self.left[0]
+                self.right = self.right[1:] + self.right[0]
+            else:
+                self.left = self.left[25] + self.left[:25]
+                self.right = self.right[25] + self.right[:25]
+
+    def rotate_to_letter(self, letter):
+        n = self.left.find(letter)
+        self.rotate(n)
+
+    def set_ring(self, n):
+
+        # rotate the rotor backwards
+        self.rotate(n-1, forward=False)
+
+        # adjust turnover notch in accordance with wiring
+        n_notch = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".find(self.notch)
+        self.notch = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[(n_notch - (n + 1)) & 26]
